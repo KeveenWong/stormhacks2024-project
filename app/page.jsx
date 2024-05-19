d96e7637922306fdc0f5f7a16a4ebabaa26b7ed5
Sunny Li <sli079@mylangara.ca>
2024-05-18 17:07:09 -0700

    Main page finished

--- app/page.jsx
+++ app/page.jsx
@@ -1,20 +1,23 @@
-'use client';
-// pages/index.js
-
 import Head from 'next/head';
-import SpeechCard from '../components/SpeechCard';
+import Image from 'next/image';
+import MainPageCat from '@public/assets/images/MainPageCat.png';
 
 export default function Home() {
-  const sampleText = "Hello, this is a simulated speech being displayed in the card.";
   return (
     <div>
       <Head>
-        <title>Speech Simulation</title>
+        <title>Yoga Companion</title>
       </Head>
       <main>
-        <h1>Speech Card</h1>
-        <SpeechCard text={sampleText} />
-        <button onClick={() => console.log("Button clicked!")}>Click me</button>
+        <div className="flex items-center">
+          <h1 className="mr-4">Welcome to your yoga companion!</h1>
+          <Image
+            src={MainPageCat}
+            alt="Main Page Cat"
+            width={500}
+            height={500}
+          />
+        </div>
       </main>
     </div>
   );
